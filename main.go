6b021b1d45aca819571cc9e0d5f187ad017fc585
Michal Sekletar <msekleta@redhat.com>
2015-11-30 17:00:11 +0100

    Add handler for request on buildsrpm API endpoint
    For now just print body of the request.

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
+type apiHandler struct {
+}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatalf("Failed to read request body: %s", err)
+	}
+
+	fmt.Printf("%s", body)
+}
+
+func setupHandlers() {
+	http.Handle("/buildsrpm", apiHandler{})
+}
+
+func main() {
+	serverAddress := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
+
+	if err := http.ListenAndServe(serverAddress, nil); err != nil {
+		log.Fatalf("Failed to start the server: %s", err)
+	}
 }
