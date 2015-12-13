4cc4eee4b7f930cf50198867291db10b51c9a22c
Michal Sekletar <msekleta@redhat.com>
2015-12-13 16:09:15 +0100

    Migrate from handwritten HTTP handler to hookserve

--- main.go
+++ main.go
@@ -2,35 +2,23 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
+	"github.com/phayes/hookserve/hookserve"
 	"os"
+	"time"
 )
 
-type apiHandler struct {
-}
-
-func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatalf("Failed to read request body: %s", err)
-	}
-
-	fmt.Printf("%s", body)
-}
-
-func setupHandlers() {
-	http.Handle("/buildsrpm", apiHandler{})
-}
-
 func main() {
-	serverAddress := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
+	server := hookserve.NewServer()
+	server.Port = os.Getenv("OPENSHIFT_GO_PORT")
+	server.Secret = "circus$Alert=shiver"
+	server.GoListenAndServe()
 
-	setupHandlers()
-
-	if err := http.ListenAndServe(serverAddress, nil); err != nil {
-		log.Fatalf("Failed to start the server: %s", err)
+	for {
+		select {
+		case event := <-server.Events:
+			fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
+		default:
+			time.Sleep(100)
+		}
 	}
 }
